683665dc4ec0421565c9d8a6ef2c87fbb1dd1d48
Mert Bora İnevi <mertborainevi@gmail.com>
2025-10-23 19:14:22 +0200

    Suppress embeds

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module boraini/minecraft-building-ideas-bot
 
 go 1.23.0
-
-require github.com/dozerokz/discord-webhook-go v1.0.2 // indirect
--- webhook.go
+++ webhook.go
@@ -1,11 +1,67 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
-
-	discordWebhook "github.com/dozerokz/discord-webhook-go"
+	"net/http"
 )
+
+type Webhook struct {
+	Username  string `json:"username,omitempty"`
+	Content   string `json:"content,omitempty"`
+	AvatarUrl string `json:"avatar_url,omitempty"`
+	Flags     int    `json:"flags,omitempty"`
+}
+
+func CreateWebhook() Webhook {
+	return Webhook{
+		Username:  "",
+		Content:   "",
+		AvatarUrl: "",
+		Flags:     0,
+	}
+}
+
+func (w Webhook) SetContent(content string) Webhook {
+	w.Content = content
+	return w
+}
+
+func (w Webhook) SetUsername(username string) Webhook {
+	w.Username = username
+	return w
+}
+
+func (w Webhook) SetAvatarUrl(avatarUrl string) Webhook {
+	w.AvatarUrl = avatarUrl
+	return w
+}
+
+func (w Webhook) SuppressEmbeds() Webhook {
+	w.Flags = w.Flags | 4
+	return w
+}
+
+func (w Webhook) Post(webhookUrl string) (Webhook, error) {
+	data, err := json.Marshal(w)
+	if err != nil {
+		return w, fmt.Errorf("failed to post webhook: %w", err)
+	}
+	resp, err := http.Post(webhookUrl, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		return w, fmt.Errorf("failed to post webhook: %w", err)
+	}
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(string(b))
+	defer resp.Body.Close()
+	return w, nil
+}
 
 func PostBuildingIdea(idea BuildingIdea, config Config) {
 	// Create a webhook
@@ -15,24 +71,20 @@ func PostBuildingIdea(idea BuildingIdea, config Config) {
 	} else {
 		link = idea.SourceTitle
 	}
-	webhook, err := discordWebhook.CreateWebhook(fmt.Sprintf(`
+	_, err := CreateWebhook().SuppressEmbeds().
+		SetContent(fmt.Sprintf(`
 <@&1407712465868820530>
 This Week's Building Idea
 # %s
 -# Source: %s
 `,
-		idea.Title,
-		link,
-	),
-		"Minecraft Building Ideas",
-		"",
-	) // replace with actual image url (string)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Send the webhook
-	err = discordWebhook.SendWebhook(config.WebhookUrl, webhook)
+			idea.Title,
+			link,
+		)).
+		SetUsername(
+			"Minecraft Building Ideas",
+		).
+		Post(config.WebhookUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
